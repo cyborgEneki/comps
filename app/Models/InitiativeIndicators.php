2fc5041141d5ee66fd16f6e01ff95f3cf1c61532
Eneki <joanreneki@gmail.com>
2020-11-21 19:23:59 +0300

    Create dimension and fact table models and corresponding controllers

---
+++ app/Models/InitiativeIndicators.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InitiativeIndicators extends Model
+{
+    use HasFactory;
+}
