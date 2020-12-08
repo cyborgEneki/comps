fdd773558010d0d58137c9d597db42d53c519101
Eneki <joanreneki@gmail.com>
2020-12-08 08:38:30 +0300

    Clean up Home view

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,33 +1,11 @@
 <template>
-  <div>
-    <SelectGoalTeam :goalTeamIdFromDbProp="goalTeamIdFromDb" @fetchGoalTeamIdFromUser="cacheGoalTeamIdFromUser"></SelectGoalTeam>
-   
-  </div>
+  <div></div>
 </template>
 
 <script>
-import SelectGoalTeam from "../views/SelectGoalTeam";
-
 export default {
   data() {
-    return {
-      goalTeamIdFromUser: null,
-      initiative: null,
-      goalTeamIdFromDb: null
-    };
+    return {};
   },
-
-  components: {
-    SelectGoalTeam
-  },
-
-  methods: {
-    cacheGoalTeamIdFromUser(value) {
-      this.goalTeamIdFromUser = value;
-    },
-    cacheInitiativeGoalTeamIdFromDb(value) {
-      this.goalTeamIdFromDb = value
-    }
-  }
 };
 </script>
